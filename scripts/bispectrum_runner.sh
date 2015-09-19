5388d6582016c9fd8934c142efe22cd7a25e6008
Vera Gluscevic <vera.gluscevic@gmail.com>
2015-09-19 05:00:39 -0400

    putting sim number into filename

--- scripts/bispectrum_runner.sh
+++ scripts/bispectrum_runner.sh
@@ -13,5 +13,5 @@ export ihi=$[$ilo + 24]
 
 for i in `seq $ilo $ihi`;
 do 
-    mpirun python calc_bispectrum.py --alm1 sim${i}_planck_353GHz_tlm.npy --alm2 sim${i}_planck_353GHz_elm.npy --alm3 sim${i}_planck_353GHz_blm.npy --filename b_T353_E353_B353_lmax100.npy --lmax 100
+    mpirun python calc_bispectrum.py --alm1 sim${i}_planck_353GHz_tlm.npy --alm2 sim${i}_planck_353GHz_elm.npy --alm3 sim${i}_planck_353GHz_blm.npy --filename sim${i}_b_T353_E353_B353_lmax100.npy --lmax 100
 done
