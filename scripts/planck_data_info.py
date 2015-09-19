5322f1adb77d79653417f8f3696e80b910deed89
Vera Gluscevic <vera.gluscevic@gmail.com>
2015-09-19 00:07:55 -0400

    added planck info;

---
+++ scripts/planck_data_info.py
@@ -0,0 +1,25 @@
+
+# for beam_file='HFI_RIMO_Beams-100pc_R2.00.fits'
+BEAM_INDEX = {
+    '100': 3,
+    '143': 4,
+    '217': 5,
+    '353': 6,
+    '100P': 7,
+    '143P': 8,
+    '217P': 9,
+    '353P': 10,
+    }
+    
+
+#for maskfile='HFI_Mask_GalPlane-apo{}_2048_R2.00.fits'
+MASK_FIELD = {
+    20: 0,
+    40: 1,
+    60: 2,
+    70: 3,
+    80: 4,
+    90: 5,
+    97: 6,
+    99: 7,
+}
