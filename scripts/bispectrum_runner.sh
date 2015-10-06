f08c4ed406ffaec5709858b8d9b4c4c654550505
Vera Gluscevic <vera.gluscevic@gmail.com>
2015-10-06 02:23:24 -0400

    fix runs

--- scripts/bispectrum_runner.sh
+++ scripts/bispectrum_runner.sh
@@ -14,6 +14,6 @@ export lmax=200
 
 for i in `seq $ilo $ihi`;
 do 
-    mpirun python calc_bispectrum.py --alm1 sim${i}_planck_353GHz_lmax${lmax}_tlm.npy --alm2 sim${i}_planck_353GHz_lmax${lmax}_elm.npy --alm3 sim${i}_planck_353GHz_lmax${lmax}_blm.npy --filename sim${i}_T353_E353_B353_lmax${lmax}.npy --lmax $lmax
+    mpirun python calc_bispectrum.py --alm1 sim${i}_planck_353GHz_lmax${lmax}_Tlm.npy --alm2 sim${i}_planck_353GHz_lmax${lmax}_Elm.npy --alm3 sim${i}_planck_353GHz_lmax${lmax}_Blm.npy --filename sim${i}_T353_E353_B353_lmax${lmax}.npy --lmax $lmax
     
 done
