cc197f9e5914448db4486574f6d7fd1747f23bc2
Vera Gluscevic <vera.gluscevic@gmail.com>
2015-10-06 02:16:54 -0400

    fix calc b

--- scripts/bispectrum_runner.sh
+++ scripts/bispectrum_runner.sh
@@ -10,16 +10,10 @@
 
 export ilo=$[$[$SGE_TASK_ID - 1] * 5 + 1]
 export ihi=$[$ilo + 4]
+export lmax=200
 
 for i in `seq $ilo $ihi`;
 do 
-    #mpirun python calc_bispectrum.py --alm1 sim${i}_planck_353GHz_tlm.npy --alm2 sim${i}_planck_353GHz_elm.npy --alm3 sim${i}_planck_353GHz_blm.npy --filename sim${i}_b_T353_E353_B353_lmax100.npy --lmax 100
-    #mpirun python calc_bispectrum.py --alm1 sim${i}_planck_143GHz_tlm.npy --alm2 sim${i}_planck_353GHz_elm.npy --alm3 sim${i}_planck_353GHz_blm.npy --filename sim${i}_b_T143_E353_B353_lmax100.npy --lmax 100
-
-    mpirun python calc_bispectrum.py --alm1 sim${i}_planck_143GHz_tlm.npy --alm2 sim${i}_planck_143GHz_elm.npy --alm3 sim${i}_planck_143GHz_blm.npy --filename sim${i}_b_T143_E143_B143_lmax100.npy --lmax 100
-
-    mpirun python calc_bispectrum.py --alm1 sim${i}_planck_353GHz_tlm.npy --alm2 sim${i}_planck_143GHz_elm.npy --alm3 sim${i}_planck_143GHz_blm.npy --filename sim${i}_b_T353_E143_B143_lmax100.npy --lmax 100
-
-    mpirun python calc_bispectrum.py --alm1 sim${i}_planck_353GHz_tlm.npy --alm2 sim${i}_planck_217GHz_elm.npy --alm3 sim${i}_planck_217GHz_blm.npy --filename sim${i}_b_T353_E217_B217_lmax100.npy --lmax 100
-
+    mpirun python calc_bispectrum.py --alm1 sim${i}_planck_353GHz_lmax${lmax}_tlm.npy --alm2 sim${i}_planck_353GHz_lmax${lmax}_elm.npy --alm3 sim${i}_planck_353GHz_lmax${lmax}_blm.npy --filename sim${i}_T353_E353_B353_lmax${lmax}.npy --lmax $lmax
+    
 done
