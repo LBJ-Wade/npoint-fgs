4da14d2fc567955f274e4bd15168ae2ec524ac20
Vera Gluscevic <verag@ias.edu>
2016-01-05 21:35:51 -0500

    stuff after applications?

--- scripts/bispectrum_runner_nompi.sh
+++ scripts/bispectrum_runner_nompi.sh
@@ -9,6 +9,6 @@
 export i=$SGE_TASK_ID
 export lmax=200
 
-mpirun python calc_bispectrum.py --alm1 sim${i}_planck_353GHz_lmax${lmax}_Tlm.npy --alm2 sim${i}_planck_353GHz_lmax${lmax}_Elm.npy --alm3 sim${i}_planck_353GHz_lmax${lmax}_Blm.npy --filename sim${i}_T353_E353_B353_lmax${lmax}.npy --lmax $lmax
+python calc_bispectrum.py --alm1 sim${i}_planck_353GHz_lmax${lmax}_Tlm.npy --alm2 sim${i}_planck_353GHz_lmax${lmax}_Elm.npy --alm3 sim${i}_planck_353GHz_lmax${lmax}_Blm.npy --filename sim${i}_T353_E353_B353_lmax${lmax}.npy --lmax $lmax
     
 
